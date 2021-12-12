7c74d41a832aad5017672f3c67481b455485a9b1
Pablo Fontanilla <chichaslocas@gmail.com>
2021-12-12 01:20:30 +0100

    Final version AoC3

--- aoc3/main.py
+++ aoc3/main.py
@@ -2,33 +2,38 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import collections
 import sys
-from array import *
-from functools import reduce
-from typing import Tuple
 
 
-def parse_line(raw_line) -> tuple[str, int]:
-    parsed_line = raw_line.split(' ')
-    return parsed_line[0], int(parsed_line[1])
 
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    with open('input.txt') as input_file: #0,9 -> 2,9
-
-        horizontal_position = 0
-        depth = 0
-
+    filename = 'input.txt'
+    bit_number = 5 if filename == 'test.txt' else 12
+    bits = []
+    binary_gamma_rate = []
+    binary_epsilon_rate = []
+    for index in range (0,bit_number):
+        bits.append([])
+    with open(filename) as input_file:
         for line in input_file:
-            direction, distance = parse_line(line)
-            match direction:
-                case 'forward':
-                    horizontal_position+=distance
-                case 'down':
-                    depth+=distance
-                case 'up':
-                    depth-=distance
+            for index in range(0, bit_number):
+                bits[index].append(line[index])
+
+        for position in bits:
+            most_common = collections.Counter(position).most_common((1))[0][0]
+            binary_gamma_rate.append(most_common)
+            binary_epsilon_rate.append('0' if most_common == '1' else '1')
+
+        binary_gamma_rate = ''.join(str(bit) for bit in binary_gamma_rate)
+        binary_epsilon_rate = ''.join(str(bit) for bit in binary_epsilon_rate)
+        int_gamma_rate = int(str(binary_gamma_rate),2)
+        int_epsilon_rate = int(str(binary_epsilon_rate), 2)
 
 
-        print(horizontal_position*depth)
+        print(int_gamma_rate*int_epsilon_rate)
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- aoc3/main2.py
+++ aoc3/main2.py
@@ -2,35 +2,81 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import sys
-from array import *
-from functools import reduce
-from typing import Tuple
+import collections
 
 
-def parse_line(raw_line) -> tuple[str, int]:
-    parsed_line = raw_line.split(' ')
-    return parsed_line[0], int(parsed_line[1])
+def calculate_binary_oxygen_criteria(column,bit_position):
 
+    frequency = collections.Counter(column).most_common(2)
+    if frequency[0][1] == frequency[1][1]:
+        most_common = '1'
+    else:
+        most_common = frequency[0][0]
+    return most_common
+
+
+def calculate_binary_co2_criteria(candidates,bit_position):
+
+    criteria = '0' if calculate_binary_oxygen_criteria(candidates,bit_position) == '1' else '1'
+    return criteria
+
+
+def get_column(full_input,position):
+    result = [item[position] for item in full_input]
+    return result
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    with open('input.txt') as input_file: #0,9 -> 2,9
-
-        horizontal_position = 0
-        depth = 0
-        aim = 0
-
+    filename = 'input.txt'
+    bit_number = 5 if filename == 'test.txt' else 12
+    bits = []
+    binary_oxygen_criteria = []
+    binary_co2_criteria = []
+    oxygen_rate_candidates = []
+    co2_rate_candidates = []
+    oxygen_rate = '0'
+    co2_rate = '0'
+    for index in range (0,bit_number):
+        bits.append([])
+    with open(filename) as input_file:
+        #Fill initial candidates
         for line in input_file:
-            direction, value = parse_line(line)
-            match direction:
-                case 'forward':
-                    horizontal_position+=value
-                    depth+=(value*aim)
-                case 'down':
-                    aim+=value
-                case 'up':
-                    aim-=value
+            clean_line = line.strip('\n')
+            oxygen_rate_candidates.append(clean_line)
+            co2_rate_candidates.append(clean_line)
 
 
-        print(horizontal_position*depth)
+        #calculate oxygen rate
+        next_oxygen_rate_candidates = []
+
+        for index in range (0,bit_number):
+            candidates = get_column(oxygen_rate_candidates,index)
+            oxygen_criteria_bit = calculate_binary_oxygen_criteria(candidates,index)
+            for candidate in oxygen_rate_candidates:
+                candidate_bit = candidate[index]
+                if candidate_bit == oxygen_criteria_bit:
+                    next_oxygen_rate_candidates.append(candidate)
+            oxygen_rate_candidates = next_oxygen_rate_candidates
+            next_oxygen_rate_candidates = []
+            if len(oxygen_rate_candidates) == 1:
+                oxygen_rate = oxygen_rate_candidates[0]
+                break
+
+        # co2 index
+        next_co2_rate_candidates = []
+        for index in range(0, bit_number):
+            candidates = get_column(co2_rate_candidates, index)
+            co2_criteria_bit = calculate_binary_co2_criteria(candidates, index)
+            for candidate in co2_rate_candidates:
+                candidate_bit = candidate[index]
+                if candidate_bit == co2_criteria_bit:
+                    next_co2_rate_candidates.append(candidate)
+            co2_rate_candidates = next_co2_rate_candidates
+            next_co2_rate_candidates = []
+            if len(co2_rate_candidates) == 1:
+                co2_rate = co2_rate_candidates[0]
+                break
+        print(int(str(co2_rate),2)*int(str(oxygen_rate),2))
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
